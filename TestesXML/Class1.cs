9a5cbcbd313b5d588f1279e0ca73d3aeee140933
Eliel Vitor <elielvitor2020tb@gmail.com>
2023-11-07 12:21:42 -0300

    Commit para fazer as tasks em casa

---
+++ TestesXML/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeituraXmlConsole;
+using LeituraXmlConsole.utils;
+namespace TestesXML
+{
+    public class Class1
+    {
+        [Fact]
+        public void teste()
+        {
+            ScheduleDay s = new ScheduleDay();
+            int resultado = 5 + 5;
+            Assert.Equal(10, resultado);
+        }
+    }
+}
---
+++ TestesXML/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestesXML
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
